1ba7e524a0bae86c7168479a21fce999602a65df
SrLillo <jlmlillo@hotmail.com>
2024-05-20 11:56:00 +0200

    Construcción de la solicutud API - Client-Request-Response

---
+++ src/main/java/com/aluracursos/literalura/modelo/Autor.java
@@ -0,0 +1,7 @@
+package com.aluracursos.literalura.modelo;
+
+public class Autor {
+    private String nombre;
+    private String fechaDeNacimiento;
+    private String fechaDeFallecimiento;
+}
---
+++ src/main/java/com/aluracursos/literalura/modelo/Libro.java
@@ -0,0 +1,14 @@
+package com.aluracursos.literalura.modelo;
+
+import java.util.List;
+
+public class Libro {
+    private String titulo;
+    private List<String> temas;
+    private List<Autor> autores;
+    private List<String> estanterías;
+    private List<String> idiomas;
+    private String tipoDeMedio;
+    private List<String> formatos;
+    private Double numeroDeDescargas;
+}
